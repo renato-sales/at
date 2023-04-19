6fe5f0c9b630246426262cad243a4cb013e86408
renato-sales <101156709+renato-sales@users.noreply.github.com>
2023-04-19 16:02:46 -0300

    Update at.py

--- at.py
+++ at.py
@@ -3,16 +3,16 @@ quantidadePessoas = int(input("Digite a quantidade de pessoas no veículo: "))
 pesoBruto = int(input("Digite a quantidade de peso no veículo: "))
 
 if quantidadeRodas > 1 and quantidadeRodas < 4:
-	print("Veículos com duas ou três rodas")
+	print("Categoria A")
 
 elif quantidadeRodas == 4 and quantidadePessoas < 9 and pesoBruto <= 3500:
-	print("Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg;")
+	print("Categoria B")
 
 elif quantidadeRodas >= 4 and (pesoBruto >= 3500 and pesoBruto <= 6000):
-	print("Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg")
+	print("Categoria C")
 
 elif quantidadeRodas >= 4 and quantidadePessoas > 8:
-	print("Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas")
+	print("Categoria D")
 
 elif quantidadeRodas >= 4 and pesoBruto > 6000:
-	print("Veículos com quatro rodas ou mais e com mais de 6000 kg")
+	print("Categoria E")
